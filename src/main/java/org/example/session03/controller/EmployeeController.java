5f3e93f0167150ba63436e30391cb3cb56e91399
La The Phong <latp1102@gmail.com>
2026-04-07 14:41:39 +0700

    update salary

--- src/main/java/org/example/session03/controller/EmployeeController.java
+++ src/main/java/org/example/session03/controller/EmployeeController.java
@@ -11,9 +11,9 @@ public class EmployeeController {
     @GetMapping("/employee-list")
     public String listEmployees(Model model) {
         List<Employee> employees = List.of(
-                new Employee(1, "Tran Van A", "CNTT", 1000000),
-                new Employee(2, "Tran Van B", "ATTT", 1200000),
-                new Employee(3, "Tran Van C", "DTVT", 1100000)
+                new Employee(1, "Tran Van A", "CNTT", 10000),
+                new Employee(2, "Tran Van B", "ATTT", 12000),
+                new Employee(3, "Tran Van C", "DTVT", 11000)
         );
         model.addAttribute("employee", employees);
         return "employee-list";
